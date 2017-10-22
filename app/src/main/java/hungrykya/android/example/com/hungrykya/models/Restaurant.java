200fa44ea15759d807eddc3ad1ccc128d98b10ad
Payal Menon <payal.menon@asurion.com>
2017-10-21 22:24:17 -0700

    added suport for map and filter

--- app/src/main/java/hungrykya/android/example/com/hungrykya/models/Restaurant.java
+++ app/src/main/java/hungrykya/android/example/com/hungrykya/models/Restaurant.java
@@ -2,19 +2,31 @@ package hungrykya.android.example.com.hungrykya.models;
 
 public class Restaurant {
 
-    private Integer mImageUrl;
-    private Integer mTitle;
+    private String mImageUrl;
+    private String mTitle;
+    private String mRating;
+    private String mDistance;
 
-    public Restaurant(Integer imageUrl, Integer title) {
+    public Restaurant(String imageUrl, String title, String distance, String rating) {
         this.mImageUrl = imageUrl;
         this.mTitle = title;
+        this.mRating = rating;
+        this.mDistance = distance;
     }
 
-    public Integer getImageUrl() {
+    public String getImageUrl() {
         return mImageUrl;
     }
 
-    public Integer getTitle() {
+    public String getTitle() {
         return mTitle;
+    }
+
+    public String getRating() {
+        return mRating;
+    }
+
+    public String getDistance() {
+        return mDistance;
     }
 }
